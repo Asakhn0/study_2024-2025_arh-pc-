b6af0fd7cd7aaeaf2d8fa47e90ecdbc54ded7d77
Asakhn0 <saxno.06@icloud.com>
2024-10-19 17:33:52 +0300

    feat(main): add files lab-4

---
+++ labs/lab04/hello.asm
@@ -0,0 +1,19 @@
+; hello.asm
+SECTION .data                  ; Начало секций данных
+    hello: DB 'Hello word', 10 ; 'Hello word!' плюс
+                               ; символ перевода строки
+    helloLen EQU $-hello       ; Длина строки hello
+    
+SECTION .text         ; Начало секции кода
+    GLOBAL _start
+    
+_start:               ; Точка входа в программу 
+    mov eax, 4        ; Сиситемный вывоз для записи (sys_write)
+    mov ebx, 1        ; Описатель файла '1' - строительный вывод
+    mov ecx, hello    ; Адрес строки hello в ecx
+    mov edx, helloLen ; Размер строки hello
+    int 80h           ; Вывоз ядра
+    
+    mov eax, 1        ; Системный вывоз для выхода(sys_exit)
+    mov ebx, 0        ; Выход с кодом возврата '0' (без ошибок)
+    int 80h           ; Вызов ядра
